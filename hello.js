928e14a0ef31a408527cd7e6f3af7e06e961efd7
root <root@adm4.ubuntutest>
2018-04-11 01:33:43 +0500

    hello

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hellooo World\n');
 }).listen(8080, 'localhost');
 console.log('Server running at http://localhost:8080/');
 
